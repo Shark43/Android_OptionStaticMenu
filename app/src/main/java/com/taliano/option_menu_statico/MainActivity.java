8cc86de60d973951e9ba5e63de3e180d1fde3f40
Shark43 <talia4343@gmail.com>
2021-03-21 15:16:42 +0100

    first commit

---
+++ app/src/main/java/com/taliano/option_menu_statico/MainActivity.java
@@ -0,0 +1,33 @@
+package com.taliano.option_menu_statico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = this.getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(MainActivity.this, item.getTitle().toString(), Toast.LENGTH_LONG);
+        if(item.isCheckable())
+            item.setChecked(!item.isChecked());
+
+        return super.onOptionsItemSelected(item);
+    }
+}
